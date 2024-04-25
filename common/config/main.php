8769574a7900493efcfec116aca30ed5d110d0d4
mwanginjamb <fnjambi@outlook.com>
2024-04-25 09:55:22 +0300

    Feat: add an env file to manage sensitive credentials

--- common/config/main.php
+++ common/config/main.php
@@ -12,9 +12,9 @@ return [
         ],
         'db' => [
             'class' => \yii\db\Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=certificates',
-            'username' => 'root',
-            'password' => 'francis123',
+            'dsn' => 'mysql:host=' . env('DB_HOST') . ';dbname=' . env('DB_NAME'),
+            'username' => env('DB_USER'),
+            'password' => env('DB_PASSWORD'),
             'charset' => 'utf8',
         ],
     ],
