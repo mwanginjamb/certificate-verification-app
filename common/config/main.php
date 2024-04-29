b559d6f93eab9d60687b85ee4ab7f10e57ca22f1
mwanginjamb <fnjambi@outlook.com>
2024-04-29 11:04:40 +0300

    chore: remove .env from cvs

--- common/config/main.php
+++ common/config/main.php
@@ -17,5 +17,19 @@ return [
             'password' => env('DB_PASSWORD'),
             'charset' => 'utf8',
         ],
+        'mailer' => [
+            'class' => \yii\symfonymailer\Mailer::class,
+            'viewPath' => '@common/mail',
+            'useFileTransport' => false,
+
+            'transport' => [
+                'scheme' => 'smtps',
+                'host' => env('SMTP_HOST'),
+                'username' => env('SMTP_USERNAME'),
+                'password' => env('SMTP_PASSWORD'),
+                'port' => env('SMTP_PORT'),
+                'dsn' => 'smtp://' . env('SMTP_USERNAME') . ':' . env('SMTP_PASSWORD') . '@smtp.office365.com:' . env('SMTP_PORT'),
+            ],
+        ],
     ],
 ];
